15c778b74fbce1c2d018f3ee84700a515913dfac
kkk <batinae@inbox.ru>
2022-08-25 02:35:32 +0700

    Homework1

---
+++ Homework1/Program.cs
@@ -0,0 +1,94 @@
+﻿//Задача 2: Напишите программу, 
+//которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+/*
+int num1;
+int num2;
+
+Console.Write("Введите 1 число: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите 2 число: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1>num2)
+{
+    Console.Write("max = ");
+    Console.WriteLine(num1);
+    Console.Write("min = ");
+    Console.WriteLine(num2);
+}
+else {
+    Console.Write("max = ");
+    Console.WriteLine(num2);
+    Console.Write("min = ");
+    Console.WriteLine(num1);
+}
+*/
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+/*
+int num1;
+int num2;
+int num3;
+
+Console.Write("Введите 1 число: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите 2 число: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите 3 число: ");
+num3 = Convert.ToInt32(Console.ReadLine());
+
+if (num1>num2 && num1>num3)
+{
+    Console.Write("max = ");
+    Console.WriteLine(num1);}
+else if (num2>num1 && num2>num3)
+{
+    Console.Write("max = ");
+    Console.WriteLine(num2);
+}
+else if (num3>num1 && num3>num2)
+{
+    Console.Write("max = ");
+    Console.WriteLine(num3);
+}
+*/
+//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+/*
+int num;
+
+Console.Write("Введите число: ");
+num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2  == 1)
+{
+    Console.WriteLine("Число нечетное");
+}
+else
+{
+    Console.WriteLine("Число четное");
+}
+*/
+// Задача 8: Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+
+/*int num;
+
+Console.Write("Введите число: ");
+num = Convert.ToInt32(Console.ReadLine());
+int current = 1;
+
+while (current <= num)
+  
+{
+    if (current %2 == 0)
+    
+   {
+     Console.Write(current + " ");
+   }
+    current = current+1;
+}
+*/
+
